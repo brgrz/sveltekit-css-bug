8c97827f5638f9154a3708e837f2cf6b15cabc2e
brgrz <marko@centiva.si>
2021-11-27 11:13:16 +0100

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,31 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<sl-card class="card-overview">
+	<img slot="image" src="https://placeimg.com/640/480/nature/grayscale" alt="alt" />
+
+	<strong>Mittens</strong><br />
+	This kitten is as cute as he is playful. Bring him home today!<br />
+	<small>6 weeks old</small>
+
+	<div slot="footer">
+		<sl-button type="primary" pill>More Info</sl-button>
+		<sl-rating />
+	</div>
+</sl-card>
+
+<style>
+	.card-overview {
+		width: 300px;
+	}
+
+	.card-overview small {
+		color: var(--sl-color-neutral-500);
+	}
+
+	.card-overview [slot='footer'] {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+</style>
